2e01b96123a641aea3788146f4f043681d422ae4
gerard <12766134+scripting-drafts@users.noreply.github.com>
2024-04-07 02:31:28 +0200

    ...

--- Turquoise_Logger.py
+++ Turquoise_Logger.py
@@ -6,6 +6,9 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 os.system('')
 
 class Logger:
+    def __init__(self):
+        self.script_name = 'asd'
+
     def RGB(self, red=None, green=None, blue=None, bg=False):
         '''Logger prettifier'''
         if(bg == False and red != None and green != None and blue != None):
@@ -16,15 +19,15 @@ class Logger:
             return '\u001b[0m'
 
     def logging(self):
-        '''Reusable logger for QALib'''
-        test_name = str(datetime.today().strftime("%d-%m-%Y_%H-%M-%S"))
+        '''Reusable logger'''
+        # defaults = str(datetime.today().strftime("%d-%m-%Y_%H-%M-%S"))
         g0 = self.RGB()
         g1 = self.RGB(127, 255, 212)
         g2 = self.RGB(0, 0, 128)
         bold = "\033[1m"
         reset = "\033[0m"
-        logging.basicConfig(filename='QALib_debug.log', encoding='UTF-8', level=logging.DEBUG, format='%(asctime)s [%(name)s] %(message)s')
-        logger = logging.getLogger('QALib')
+        logging.basicConfig(filename='log_toast.log', encoding='UTF-8', level=logging.DEBUG, format='%(asctime)s [%(name)s] %(message)s')
+        logger = logging.getLogger('self.script_name')
         logger.setLevel(logging.DEBUG)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
